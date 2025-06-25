9a7c801f0a8603963929f5f2f4f1b760f3c2a539
AntonioSouza <antonio.claudio.souza@gmail.com>
2025-06-25 17:23:39 -0300

    Remove função waitForDB e altera porta do servidor para 5000

--- main.go
+++ main.go
@@ -7,25 +7,12 @@ import (
 	_ "minha-api/docs" // ajuste para o nome do seu módulo
 	"minha-api/routes"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
-
-func waitForDB(maxAttempts int, delay time.Duration) {
-	for i := 1; i <= maxAttempts; i++ {
-		err := database.PingDB()
-		if err == nil {
-			return
-		}
-		log.Printf("Tentativa %d: Banco ainda não disponível (%v)", i, err)
-		time.Sleep(delay)
-	}
-	log.Fatal("Banco de dados não respondeu após várias tentativas.")
-}
 
 func main() {
 	// Só carrega o .env fora do Docker
@@ -36,14 +23,11 @@ func main() {
 		}
 	}
 
-	// Aguarda o banco de dados ficar disponível de forma robusta
-	waitForDB(15, 2*time.Second)
-
 	fmt.Println("Inicializando a API...")
 	database.InitDB()
 	defer database.CloseDB()
 
 	r := routes.SetupRoutes()
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	r.Run() // Inicia o servidor na porta padrão 8080 (ou a porta definida na variável de ambiente PORT)    docker compose restart
+	r.Run(":5000") // Inicia o servidor na porta padrão 5000
 }
